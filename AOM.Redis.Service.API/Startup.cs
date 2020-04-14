0e7d695ce501276bd39aaad103923d9c61d56ca1
Adhan de Oliveira Maldonado <3843070+adhan1983@users.noreply.github.com>
2020-04-13 22:24:13 -0300

    files

---
+++ AOM.Redis.Service.API/Startup.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using StackExchange.Redis;
+
+namespace AOM.Redis.Service.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Redis API", Version = "v1" });
+                c.IgnoreObsoleteActions();
+                c.IgnoreObsoleteProperties();
+                c.DocumentFilterDescriptors.AsReadOnly();
+                c.CustomSchemaIds(i => i.FullName);                
+            });
+            
+            var redis = ConnectionMultiplexer.Connect("redis-003.kuk7ny.0001.use2.cache.amazonaws.com");
+            
+            services.AddScoped<IDatabase>(rd => redis.GetDatabase());
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("v1/swagger.json", "Redis.API v1");
+                c.DefaultModelExpandDepth(0);
+                c.DefaultModelsExpandDepth(-1);
+            });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
