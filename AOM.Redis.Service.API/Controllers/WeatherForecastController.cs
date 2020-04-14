a4a000877d14efc4084f5a52aa162945b8e4679f
Adhan de Oliveira Maldonado Maldonado <adhan.maldonado@wooza.com.br>
2020-04-14 15:18:04 -0300

    create endpoints

--- AOM.Redis.Service.API/Controllers/WeatherForecastController.cs
+++ AOM.Redis.Service.API/Controllers/WeatherForecastController.cs
@@ -3,10 +3,7 @@ using AOM.Redis.Service.DataAccess;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Options;
-using StackExchange.Redis;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace AOM.Redis.Service.API.Controllers
 {
@@ -17,54 +14,79 @@ namespace AOM.Redis.Service.API.Controllers
         private readonly RedisConfiguration _redis;
         private readonly IDistributedCache _cache;
         private readonly IRedisConnectionFactory _fact;
+        private RedisVoteService<Person> _redisVoteService;
         public WeatherForecastController(IOptions<RedisConfiguration> redis, IDistributedCache cache, IRedisConnectionFactory factory) 
         {            
             _redis = redis.Value;
             _cache = cache;
             _fact = factory;
+            _redisVoteService = new RedisVoteService<Person>(_fact);
         }
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        //private static readonly string[] Summaries = new[]
+        //{
+        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //};
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+
+        //    _cache.SetString("CacheTest", "Redis is awesome");
+
+        //    var _value = _cache.GetString("CacheTest");
+
+        //    var db = _fact.Connection().GetDatabase();
+
+        //    db.StringSet("StackExchange.Redis.Key", "Stack Exchange Redis is Awesome");
+
+        //    _value = db.StringGet("StackExchange.Redis.Key");            
+
+
+
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
+        [HttpPost]
+        [Route("create")]
+        public string Post([FromBody]Person person)
         {
             var rng = new Random();
 
-            _cache.SetString("CacheTest", "Redis is awesome");
+            Person model = new Person();
+            model.Name = person.Name;
+            model.Id = rng.Next(1, 55).ToString();
 
-            var _value = _cache.GetString("CacheTest");
+            _redisVoteService.Save(model.Id.ToString(), model);
 
-            var db = _fact.Connection().GetDatabase();
-            
-            db.StringSet("StackExchange.Redis.Key", "Stack Exchange Redis is Awesome");
-
-            _value = db.StringGet("StackExchange.Redis.Key");            
-
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return model.Id;
         }
 
-        [HttpPost]
-        public void Post([FromBody]Person person) 
+        [HttpGet]
+        [Route("id")]
+        public Person Get([FromQuery]string id)
         {
-            var redis = new RedisVoteService<Person>(this._fact);
+            _redisVoteService = new RedisVoteService<Person>(this._fact);
+            
+            var model = _redisVoteService.Get(id);
 
-            redis.Save(person.Name, person);
+            return model;
+        }
 
-            var model = redis.Get(person.Name);
-
-            if (model != null) 
-            {
-                
-            }
+        [HttpDelete]
+        [Route("/{id}")]
+        public bool Delete([FromRoute]string id)         
+        {
+            _redisVoteService.Delete(id);
+            
+            return true;            
         }
     }
 }
--- AOM.Redis.Service.API/Models/Person.cs
+++ AOM.Redis.Service.API/Models/Person.cs
@@ -1,7 +1,9 @@
 ﻿namespace AOM.Redis.Service.API.Models
 {
     public class Person
-    {
+    {     
+       
+        public string Id { get; set; }
         public string Name { get; set; }
     }
 }
